f99f6f311e0f1dc837c48372ad034ebcb7d59baa
Claudio Paulo <pclaudio0816@gmail.com>
2018-03-16 13:40:08 +0000

    Setup do meu projecto completo com sucesso

---
+++ lojaOnlineBackend/src/main/java/pt/proj/ual/lojaOnlineBackend/App.java
@@ -0,0 +1,13 @@
+package pt.proj.ual.lojaOnlineBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
